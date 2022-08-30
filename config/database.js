afeec25130641b3b9e43c80e644ac521b4f31712
Guilhermeaka <guilhermedssantos1@gmail.com>
2022-08-30 10:50:11 -0300

    heroku config

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", 0.0.0.0),
-      port: env.int("DATABASE_PORT", 5432),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", process.env.PORT, "0.0.0.0"),
       database: env("DATABASE_NAME", "database"),
       user: env("DATABASE_USERNAME", "root"),
       password: env("DATABASE_PASSWORD", ""),
